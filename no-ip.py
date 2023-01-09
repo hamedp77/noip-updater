4a00bf75e8b4551eb0181a24fa24160c32406fe4
hamedp77 <117587802+hamedp77@users.noreply.github.com>
2023-01-09 20:00:17 +0330

    added dnsQuery, finished responseHandler and more
    1. Moved dnsQuery method into the same file and made some changes so it prints out an error message in case any happened.
    2. Renamed the errorHandling method to responseHandler, as it represents functionality of the method better, and covered all the response messages that No-IP API sends back with a somewhat descriptive message that explains what happened.
    3. Some minor touch ups to the overall code like using the strip method to get rid of whitespaces instead of replace.
    4. Basic error handling particularly where there's any HTTP call to some server in order to make sure we get 200 OK, if not show some error message.
    5. Added timestamp to all the messages shown in the console.
    6. Added a short keyword describing what type each message is. this will probably expand into a logging mechanism: (info, internal error, No-IP response when the request has a valid response but with error)

--- no-ip.py
+++ no-ip.py
@@ -2,39 +2,52 @@ import requests
 import datetime
 from base64 import b64encode, b64decode
 from time import sleep
-from google_doh_api import dns_query
 
 ENDPOINT = 'https://dynupdate.no-ip.com/nic/update'
 USERNAME = 'hamedp77@gmail.com'
 PASSWORD = 'h@med103070'
 HOSTNAME = 'hmdnetwork.ddns.net'
-INTERVAL = 5 # in seconds
+INTERVAL = 300 # in seconds
 AUTHSTRING = b64encode(USERNAME.encode() + ':'.encode() + PASSWORD.encode())
+
 
 def getIP():
     
-    # get current public IPv4 address of the client machine
+    # Get current public IPv4 address of the client machine
 
     ipInfoEndpoint = 'https://2ip.io'
     headers = {'User-Agent': 'curl/7.83.1'}
 
     r = requests.get(ipInfoEndpoint, headers=headers)
 
-    return r.text.replace('\n', '')
+    if r.status_code == 200:
+        return r.text.strip()
+    else:
+        print(datetime.datetime.now(), ': [internal error] An error occured while trying to retrieve machine\'s IP address.')
 
+
+def dnsQuery(name, type='a'):
+
+    r = requests.get(f'https://8.8.8.8/resolve?name={name}&type={type}')
+
+    if r.status_code == 200:
+        try:
+            return r.json()['Answer'][0]['data']
+            
+        except (IndexError, KeyError):
+            pass
+    else:
+        print(datetime.datetime.now(), ': [internal error] An error occured while trying to retreive hostname\'s IP address.')
 
 def updateHostname(newIP):
 
-    # update the hostname if any IP change is detected
+    # Update the hostname if any IP change is detected
 
     headers = {'User-Agent': 'curl/7.83.1', 'Authorization': 'Basic ' + AUTHSTRING.decode()}
 
     r = requests.get(ENDPOINT + f'?hostname={HOSTNAME}&myip={newIP}', headers=headers)
 
-    if r.status_code != 200:
-        print(datetime.datetime.now(), ': Update Failed!')
-    else:
-        errorHandling(r.text.replace('\r\n', ''))
+    responseHandler(r.text.strip())
 
 
 def checkForIpChange(interval):
@@ -45,27 +58,49 @@ def checkForIpChange(interval):
 
     '''
 
-    currentIP = dns_query(HOSTNAME, 'a')
     while True:
+        currentIP = dnsQuery(HOSTNAME)
         if getIP() != currentIP:
-            print(datetime.datetime.now(), ': New IP Detected, Updating Hostname...')
+            print(datetime.datetime.now(), ': [info] New IP Detected, Updating Hostname...')
             currentIP = getIP()
             updateHostname(currentIP)
         else:
-            print(datetime.datetime.now(), ': No IP Change Detected')
+            print(datetime.datetime.now(), ': [info] No IP Change Detected')
         sleep(interval)
 
-def errorHandling(noipResponse):
 
-    # Parse the response from No-IP and output the errors, if any occured
+def responseHandler(noipResponse):
+
+    # Parse the response from No-IP and output relevant errors or messages.
 
     if 'good' in noipResponse:
-        print(datetime.datetime.now(), ': Update Successful! ', noipResponse.split(' ')[1])
+            print(datetime.datetime.now(), ': [good] Update Successful! ', noipResponse.split(' ')[1])
+
+    elif 'nochg' in noipResponse:
+        print(datetime.datetime.now(), ': [nochg] Hostname already up to date.', noipResponse.split(' ')[1])
+
     elif 'nohost' in noipResponse:
-        print(datetime.datetime.now(), ''': Hostname supplied does not exist under specified account.
-                Please double check your information and try again.''')
+        print(datetime.datetime.now(), f': [{noipResponse}] Hostname supplied does not exist under specified account. Please double check your information and try again.')
         exit()
 
+    elif 'badauth' in noipResponse:
+        print(datetime.datetime.now(), f': [{noipResponse}] Invalid username or password. Please double check your information and try again.')
+        exit()
+
+    elif 'badagent' in noipResponse:
+        print(datetime.datetime.now(), f': [{noipResponse}] Client disabled. Contact the developer(s).')
+        exit()
+
+    elif '!donator' in noipResponse:
+        print(datetime.datetime.now(), f': [{noipResponse}] Feature not available.')
+
+    elif 'abuse' in noipResponse:
+        print(datetime.datetime.now(), f': [{noipResponse}] Username is blocked due to abuse.')
+        exit()
+
+    elif '911' in noipResponse:
+        print(datetime.datetime.now(), f': [{noipResponse}] Fatal Error Occured! Try again in 30 minutes.')
+        exit()
 
 
 checkForIpChange(INTERVAL)
