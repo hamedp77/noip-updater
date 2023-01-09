871533a614116a09ee10f4a0fc55225d110ff8e8
hamedp77 <117587802+hamedp77@users.noreply.github.com>
2023-01-09 20:03:11 +0330

    removed jsonResponse and return answer directly

--- google_doh_api.py
+++ google_doh_api.py
@@ -3,8 +3,6 @@ import json
 
 def dns_query(name, type):
 
-    req = requests.get(f'https://8.8.8.8/resolve?name={name}&type={type}')
+    r = requests.get(f'https://8.8.8.8/resolve?name={name}&type={type}')
     
-    jsonResponse = json.loads(req.content)
-
-    return jsonResponse['Answer'][0]['data']
+    return r.json()['Answer'][0]['data']
